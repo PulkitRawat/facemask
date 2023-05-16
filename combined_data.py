e057eca08ec5568e3b72bab5434b15614820f231
Pulkit <123824584+PulkitRawat@users.noreply.github.com>
2023-05-16 13:56:26 +0530

    combined and trained data

---
+++ combined_data.py
@@ -0,0 +1,17 @@
+from without_mask import wom
+from with_mask import wm
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+x = np.r_[wm,wom]
+
+labels = np.zeros(x.shape[0])
+labels[200:] = 1.0
+
+output = {0:"mask on",1:"mask off"}
+
+x_train, x_test, y_train, y_test = train_test_split(x,labels,test_size=0.25)
+y_pred = SVC.predict(x_test)  
+print(accuracy_score(y_test, y_pred))
