1d4c205572b97c62caa66ce9cb9e9758fdb9d1d5
Pulkit <123824584+PulkitRawat@users.noreply.github.com>
2023-05-16 12:11:48 +0530

    usuable for live as well as saved feed

--- facemaskdetect.py
+++ facemaskdetect.py
@@ -7,11 +7,17 @@ cascPathEyes = os.path.dirname(cv.__file__) + "/data/haarcascade_eye_tree_eyegla
 faceCascade = cv.CascadeClassifier(cascPathFace)
 eyeCascade =  cv.CascadeClassifier(cascPathEyes)
 
-# opening the webcam
-video_capture = cv.VideoCapture(0)
+# for the webcam
+# video_capture = cv.VideoCapture(0)
+
+# for the image 
+frame = cv.imread("WhatsApp Image 2023-05-11 at 13.31.53.jpeg")
+
+# for already captured video
+# video_capture = cv.VideoCapture("path_to_video")
 
 while True:
-    ret, frame = video_capture.read()
+    # ret, frame = video_capture.read() #only required in case of videos
 
     # converting to grayscale
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -23,16 +29,19 @@ while True:
     for (x,y,w,h) in faces:
         cv.rectangle(frame, (x,y), (x+w,y+h), (233, 218, 201), 2)
                         #top-left  bottom-right
-        faceROI = frame[y:y+h, x:x+w]
-        eyes  = eyeCascade.detectMultiScale(faceROI)
-        for(x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2 // 2, y + y2 + h2 // 2)  
-            # the '//' operator represents the result will be rounded of to nearest integer
-            radius = int(round((w2+h2)*0.25))
-            cv.circle(frame, eye_center, radius, (255, 221, 173),4)
-    cv.imshow('Video', frame)
+
+        #eye detection
+        # faceROI = frame[y:y+h, x:x+w]
+        # eyes  = eyeCascade.detectMultiScale(faceROI)
+        # for(x2,y2,w2,h2) in eyes:
+        #     eye_center = (x + x2 + w2 // 2, y + y2 + h2 // 2)  
+        #     # the '//' operator represents the result will be rounded of to nearest integer
+        #     radius = int(round((w2+h2)*0.25))
+        #     cv.circle(frame, eye_center, radius, (255, 221, 173),4)
+        
+    cv.imshow('result', frame)
     if cv.waitKey(1) & 0xFF == ord('q'): #escaping the if loop if 'q' is pressed
         break
     
-video_capture.release()
+# video_capture.release()
 cv.destroyAllWindows()
